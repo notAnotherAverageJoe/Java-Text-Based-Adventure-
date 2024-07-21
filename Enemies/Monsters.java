a879af1f47a982d5f3a344787a5d26862e2adb78
joseph skokan <joeskokan20@gmail.com>
2024-07-20 21:50:50 -0400

    Adding setters and getters on monster

--- Enemies/Monsters.java
+++ Enemies/Monsters.java
@@ -6,6 +6,38 @@ public class Monsters {
     private String species;
     private int health;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPowerLevel() {
+        return powerLevel;
+    }
+
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
     public Monsters(String name, int powerLevel, String species, int health) {
         this.name = name;
         this.powerLevel = powerLevel;
